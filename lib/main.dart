c675c5e3bfc52de3459503def7a46cc90433d613
vbazurtob <vbazurtob@gmail.com>
2019-04-30 14:19:34 -0700

    Flutter project files

---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:my_favorite_places/screens/home_page/home_page.dart';
+
+void main() => runApp(
+    MaterialApp(
+      title: 'My favorite places',
+      home: HomePage(),
+    )
+);
