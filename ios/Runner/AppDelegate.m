29bf1a6c27462bf2a04700c08a9eede8b82fb60e
vbazurtob <vbazurtob@gmail.com>
2019-05-01 16:18:52 -0700

    Delete files

--- ios/Runner/AppDelegate.m
+++ ios/Runner/AppDelegate.m
@@ -5,7 +5,7 @@
 
 - (BOOL)application:(UIApplication *)application
     didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-  [GMSServices provideAPIKey:@""];
+  [GMSServices provideAPIKey:@"AIzaSyBWp7YMY0hWblpXKEd4SdtaJwvBvEgmxGM"];
   [GeneratedPluginRegistrant registerWithRegistry:self];
   // Override point for customization after application launch.
   return [super application:application didFinishLaunchingWithOptions:launchOptions];
