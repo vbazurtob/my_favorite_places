b9b0ede5f83e068e0977b7290358ffb75812949f
vbazurtob <vbazurtob@gmail.com>
2019-05-01 15:46:22 -0700

    added gmaps plugin

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:my_favorite_places/screens/home_page/gmaps_demo.dart';
 import 'package:my_favorite_places/screens/home_page/home_page.dart';
 
 void main() => runApp(
     MaterialApp(
       title: 'My favorite places',
-      home: HomePage(),
+      home: MapSample(),
     )
 );
